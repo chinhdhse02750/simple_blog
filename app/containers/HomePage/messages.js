215c1505c2c347f373fca8b475560c690b041ded
chinhdhse02750 <chinhdh@ominext.com>
2022-10-04 00:35:50 +0700

    update

--- app/containers/HomePage/messages.js
+++ app/containers/HomePage/messages.js
@@ -10,6 +10,6 @@ export const scope = 'app.containers.HomePage';
 export default defineMessages({
   header: {
     id: `${scope}.header`,
-    defaultMessage: 'This is the HomePage container!',
+    defaultMessage: 'Bat dau tim hieu reactjs nghiem tuc!',
   },
 });
